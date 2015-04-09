10900184bd6fcc61e62f34ec1e4c23e118dd27bd
James E. Pustejovsky <jepusto@gmail.com>
2015-04-09 11:58:19 -0500

    Edited simulation skeleton.

--- inst/templates/Simulation-Skeleton.R
+++ inst/templates/Simulation-Skeleton.R
@@ -1,6 +1,13 @@
 rm(list = ls())
 
 #------------------------------------------------------
+# Set development values for simulation parameters
+#------------------------------------------------------
+
+# What are your model parameters?
+# What are your design parameters?
+
+#------------------------------------------------------
 # Data Generating Model
 #------------------------------------------------------
 
@@ -9,14 +16,20 @@ dgm <- function(model_params) {
   return(dat)
 }
 
+# Test the data-generating model - How can you verify that it is correct?
+
+
 #------------------------------------------------------
 # Model-fitting/estimation/testing functions
 #------------------------------------------------------
+
 
 estimate <- function(dat, design_params) {
 
   return(result)
 }
+
+# Test the estimation function
 
 #------------------------------------------------------
 # Calculate performance measures
@@ -28,6 +41,8 @@ performance <- function(results, model_params) {
 
   return(performance_measures)
 }
+
+# Check performance calculations
 
 #------------------------------------------------------
 # Simulation Driver
@@ -44,27 +59,41 @@ runSim <- function(iterations, model_params, design_params, seed = NULL) {
   performance(results, model_params)
 }
 
-source_obj <- ls()
+# demomstrate the simulation driver
+
 
 #-------------------------------------
 # Experimental Design
 #-------------------------------------
+source_obj <- ls()
 
-set.seed(20150316)
+set.seed(20150316) # change this seed value!
 
-design_factors <- list(factor1 = , factor2 = , ...)
+# now express the simulation parameters as vectors/lists
+
+design_factors <- list(factor1 = , factor2 = , ...) # combine into a design set
 params <- expand.grid(design_factors)
 params$iterations <- 5
 params$seed <- round(runif(nrow(params)) * 2^30)
 
+# All look right?
 sapply(design_factors, length)
 nrow(params)
+head(params)
 
+
+
+#--------------------------------------------------------
+# run simulations in serial
+#--------------------------------------------------------
+
+system.time(results <- mdply(params, .fun = runSim))
 
 #--------------------------------------------------------
 # run simulations in parallel
 #--------------------------------------------------------
 
+library(plyr)
 library(Pusto)
 cluster <- start_parallel(source_obj)
 
